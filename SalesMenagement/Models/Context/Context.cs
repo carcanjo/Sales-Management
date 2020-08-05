726e58d1030c7a909989c5ac3259df044fad2ef9
Cleiton Arcanjo <cleiton.arcanjo@outlook.com>
2020-08-04 21:03:06 -0300

    1º Commit

---
+++ SalesMenagement/Models/Context/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesMenagement.Models.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options){}
+        public DbSet<Departments> Departments { get; set; }
+    }
+}
---
+++ SalesMenagement/Models/Departments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesMenagement.Models
+{
+    public class Departments
+    {
+        public int DepartamentsId { get; set; }
+        public string NameDepartaments { get; set; }
+    }
+}
