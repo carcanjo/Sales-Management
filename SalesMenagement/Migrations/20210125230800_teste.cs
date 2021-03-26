11e376ea3b1c31b12332c4089659e759323cb2da
Cleiton Arcanjo <cleiton.arcanjo@outlook.com>
2021-03-26 11:30:02 -0300

    Ajuste Politica

---
+++ SalesMenagement/Migrations/20210125230800_teste.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesMenagement.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CEP",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bairro",
+                table: "Seller",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cidade",
+                table: "Seller",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Seller",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroCasa",
+                table: "Seller",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uf",
+                table: "Seller",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bairro",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "Cidade",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "NumeroCasa",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "Uf",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CEP",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
